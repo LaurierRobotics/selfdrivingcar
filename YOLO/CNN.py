044965826dda29740c3e474770aef4336b79f6b0
Laith Adi <laith_adi@hotmail.com>
2020-11-01 13:47:16 -0500

    Create CNN.py

---
+++ YOLO/CNN.py
@@ -0,0 +1,9 @@
+def yolo_cnn(img):
+    """
+    Parameters:
+        img - img we are trying to detect object 0. Size (0,0,3)
+    Returns:
+        yoloimg - smaller img with cells. Size (0,0,2,8) = (0,0,16). Each cell contains (p_c, b_x, b_y, b_h, b_w, c_1, c_2, c_3, p_c, b_x, b_y, b_h, b_w, c_1, c_2, c_3)
+    """
+    pass 
+
