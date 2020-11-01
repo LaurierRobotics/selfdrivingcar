7bd69baee0131f6de08d75bdc92d8d14e15d2c3a
Laith Adi <laith_adi@hotmail.com>
2020-11-01 13:47:20 -0500

    Create iou.py

---
+++ YOLO/nonMaxSuppression/iou.py
@@ -0,0 +1,8 @@
+def iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+    
+    Arguments:
+    box1 -- first box, list object with coordinates (box1_x1, box1_y1, box1_x2, box_1_y2)
+    box2 -- second box, list object with coordinates (box2_x1, box2_y1, box2_x2, box2_y2)
+    """
+pass
